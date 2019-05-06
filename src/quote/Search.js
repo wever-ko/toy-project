95f6650fd2bb25b129332f1ff04e662933d02c75
kukaro <justkukaro@naver.com>
2019-05-06 07:10:24 +0000

    add quote default data

---
+++ src/quote/Search.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    state = {
+
+    };
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return (
+            <input className="quote-search">
+            </input>
+        );
+    }
+}
+
+export default Search;
