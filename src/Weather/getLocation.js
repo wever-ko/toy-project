e0395b4c824f41ab7a39ee68325b38a2bf0b8ab3
Heejun Lee <zaqq1414@gmail.com>
2019-05-12 02:37:13 +0900

    filter 적용

--- src/Weather/getLocation.js
+++ src/Weather/getLocation.js
@@ -1,19 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default async function getLocation(coords) {
-    const { latitude: lat, longitude: lon } = coords;
-    
-    const { data } = await axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=${lat},${lon}&key=${process.env.REACT_APP_GOOGLE_GEO_API_KEY}`
-    );
-        
-    let city = '';
-    data.results[0].address_components.forEach(element => {
-        if(element.types.includes("sublocality_level_1")){
-            city = element.long_name;
-            return;
-        }
-    });
-    
-    return city;
+  const { latitude: lat, longitude: lon } = coords;
+
+  const { data } = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=${lat},${lon}&key=${process.env.REACT_APP_GOOGLE_GEO_API_KEY}`,
+  );
+
+  const city = data.results[0].address_components.filter((elem) => elem.types.includes('sublocality_level_1'));
+
+  console.log(city);
+
+  return city;
 }
