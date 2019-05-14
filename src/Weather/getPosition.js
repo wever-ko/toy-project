ee4d9a54213c5cf6c5b3c211ee51a1b32b959e05
Heejun Lee <zaqq1414@gmail.com>
2019-05-15 00:27:53 +0900

    change string-return to Error-throw

--- src/Weather/getPosition.js
+++ src/Weather/getPosition.js
@@ -1,5 +1,8 @@
 export default function getPosition() {
-  if (!navigator.geolocation) return 'Location not allowed';
+  if (!navigator.geolocation) {
+    throw new Error('NavigatorError');
+  }
+
   return new Promise((res, rej) => {
     navigator.geolocation.getCurrentPosition(res, rej);
   });
