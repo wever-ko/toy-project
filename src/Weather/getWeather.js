ba142dea007e3a230916b7149e141cb0f1098060
Heejun Lee <zaqq1414@gmail.com>
2019-05-15 00:39:22 +0900

    apply axios query param and return Error exception

--- src/Weather/getWeather.js
+++ src/Weather/getWeather.js
@@ -3,7 +3,18 @@ import axios from 'axios';
 export default async function getWeather(coords) {
   const { latitude: lat, longitude: lon } = coords;
 
-  return axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`,
-  );
+  try {
+    const result = axios.get('https://api.openweathermap.org/data/2.5/weather',
+      {
+        params: {
+          APPID: process.env.REACT_APP_WEATHER_API_KEY,
+          lat,
+          lon,
+          units: 'metric',
+        },
+      });
+    return result;
+  } catch (e) {
+    return new Error(e);
+  }
 }
