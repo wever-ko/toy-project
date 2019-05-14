55ef83790b939afc3e434d94401cb9f693f67fc1
Heejun Lee <zaqq1414@gmail.com>
2019-05-15 00:36:30 +0900

    apply axios query param

--- src/Weather/getLocation.js
+++ src/Weather/getLocation.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 export default async function getLocation(coords) {
   const { latitude: lat, longitude: lon } = coords;
 
-  const { data } = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng=${lat},${lon}&key=${process.env.REACT_APP_GOOGLE_GEO_API_KEY}`,
-  );
+  const { data } = await axios.get('https://maps.googleapis.com/maps/api/geocode/json',
+    {
+      params: {
+        key: process.env.REACT_APP_GOOGLE_GEO_API_KEY,
+        latlng: `${lat},${lon}`,
+      },
+    });
 
-  const city = data.results[0].address_components.filter(elem => elem.types.includes('sublocality_level_1'));
+  const city = data.results[0].address_components.filter(e => e.types.includes('sublocality_level_2'));
 
   return city;
 }
